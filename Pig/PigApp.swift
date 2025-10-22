6c28390d46c75de94edf83ce5ccd4d9b74a5a9eb
adinsmore <adinsmore@bsd220.org>
2025-10-22 10:18:06 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Alissa Dinsmore on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
